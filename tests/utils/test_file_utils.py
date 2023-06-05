d1e3deaa3fc6d48118b0c7eb0c113f2b81d397e4
rskliar <rskliar@playtika.com>
2023-06-05 21:50:25 +0300

    add test for s3 download

--- tests/utils/test_file_utils.py
+++ tests/utils/test_file_utils.py
@@ -1,8 +1,6 @@
-import pytest
 from pathlib import Path
 import tarfile
-import shutil
-import boto3
+from unittest.mock import Mock
 
 from garbage_classifier.utils.file_utils import create_train_test_split, create_tar_gz_folder, download_files_from_s3_bucket_folder, extract_tar_gz
 
@@ -43,3 +41,26 @@ def test_extract_tar_gz(tmp_path):
 
     assert extracted_path.exists()
     assert (extracted_path / 'file1.txt').exists()
+
+def test_download_files_from_s3_bucket_folder():
+    # Set up the mock S3 client
+    s3 = Mock()
+
+    # Configure the paginator to return a mock page with a mock object
+    mock_paginator = Mock()
+    mock_paginator.paginate.return_value = [
+        {'Contents': [{'Key': 'my-folder/my-file.txt'}]}
+    ]
+    s3.get_paginator.return_value = mock_paginator
+
+    # Set up the parameters
+    bucket_name = 'my-bucket'
+    folder_path = Path('my-folder')
+    local_destination = Path().absolute() / 'test-folder'
+
+    download_files_from_s3_bucket_folder(s3, bucket_name, folder_path, local_destination)
+
+    # Check that the S3 client methods were called with the expected arguments
+    s3.get_paginator.assert_called_once_with('list_objects_v2')
+    mock_paginator.paginate.assert_called_once_with(Bucket=bucket_name, Prefix=str(folder_path))
+    s3.download_file.assert_called_once_with(bucket_name, 'my-folder/my-file.txt', str(local_destination / 'my-file.txt'))
